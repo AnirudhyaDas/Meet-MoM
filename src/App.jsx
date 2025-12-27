1910484d2d1f7187468e7972a5aa7f0a3a4d38f1
Archishmaan Das <baypoint28@gmail.com>
2025-12-27 14:06:24 +0530

    Initial React project upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import logo from "./assets/logo.jpeg";
+import waveImg from "./assets/wave.jpeg";  
+
+
+function App() {
+  return (
+    <div className="container">
+
+     
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="logo" className="logo-img" />
+          <span>MoM - Minutes of Meeting</span>
+        </div>
+
+        <div className="nav-btns">
+          <button className="login-btn">Log In</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+      </nav>
+
+      
+      <section className="hero">
+        <h1>MoM is an AI-powered Meeting Summariser</h1>
+        <button className="get-started">Get Started</button>
+      </section>
+
+    
+      <div className="wave-box">
+        <img src={waveImg} alt="wave" className="wave-image" />
+      </div>
+
+      <div className="line-section">
+        <div className="line"></div>
+        <div className="play-btn">▶</div>
+        <div className="line"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
